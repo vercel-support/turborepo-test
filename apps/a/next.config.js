1df2fee5300dce8ecb021ff345669ffd4a9466ad
RCKT <orangecoloured@users.noreply.github.com>
2022-01-12 11:18:11 +0200

    Update next.config.js

--- apps/a/next.config.js
+++ apps/a/next.config.js
@@ -2,6 +2,8 @@ const { baseConfig } = require('../../base.config')
 const { webpack } = require('../../webpack.config')
 const withTM = require("next-transpile-modules")(["@turborepo-test/components"])
 
+console.log('process.env.TEST_VALUE: ', process.env.TEST_VALUE)
+
 module.exports = withTM({
     ...baseConfig,
     publicRuntimeConfig: {
